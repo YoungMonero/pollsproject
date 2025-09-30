cfefdab0ed34f3fa2368179c12c76c300a2a6a27
Menga wanji <martialwanji@gmail.com>
2025-09-30 16:02:43 +0100

    db changes

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -16,23 +16,6 @@ pool.on('error', (err) => {
 export default pool;
 
 
-// import dotenv from "dotenv";
-// import { Pool } from "pg";
-
-
-
-// dotenv.config();
-
-
-// export const pool = new Pool({
-//   user: process.env.DB_USER ,
-//   host: process.env.DB_HOST ,
-//   database: process.env.DB_NAME ,
-//   password: process.env.DB_PASSWORD ,
-//   port: parseInt(process.env.DB_PORT)
-
-
-
 export async function testConnection() {
   try {
     const client = await pool.connect();
