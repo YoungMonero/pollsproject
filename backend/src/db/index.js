90458f0df5c7cca223d6c4c95a90220b1f2d74d0
youngmonero <yungmonero29@gmail.com>
2025-09-30 12:14:26 +0100

    frontend forms

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,19 +1,19 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
-dotenv.config();
+// import pg from 'pg';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const { Pool } = pg;
+// const { Pool } = pg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
+// pool.on('error', (err) => {
+//   console.error('Unexpected error on idle client', err);
+//   process.exit(-1);
+// });
 
-export default pool;
+// export default pool;
 
 
 
